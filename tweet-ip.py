c7934cc76888601380fceb2ef41a92c91d390b41
Hackerspace Generic User <hackerspace@idi.ntnu.no>
2015-10-23 12:21:44 +0000

    Fix py3 issue with bytestrings.
    This version is tested and works with both py3 and py2 on a Pi.

--- tweet-ip.py
+++ tweet-ip.py
@@ -50,7 +50,7 @@ def get_ip():
     p = subprocess.Popen('ip route list', shell=True, stdout=subprocess.PIPE)
     data = p.communicate()
     split_data = data[0].split()
-    return split_data[split_data.index('src') + 1]
+    return split_data[split_data.index(b'src') + 1]
 
 
 try:
